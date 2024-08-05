16bffd73b5bfed4e389bb9e39c7c2e5f3e0de1ee
anirudhbelwadi <anirudh.belwadi@gmail.com>
2024-08-05 13:37:23 +0400

    import optimized

--- flask_app.py
+++ flask_app.py
@@ -1,12 +1,12 @@
 from flask import Flask
 from flask_cors import CORS
 
-app = Flask(__name__)
-
 from routes.admin import adminRoutes
 from routes.user import userRoutes
 from routes.carPool import carPoolRoutes
 from routes.uHaul import uHaulRoutes
+
+app = Flask(__name__)
 
 CORS(app)
 
